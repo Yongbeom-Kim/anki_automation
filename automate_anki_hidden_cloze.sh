ed4db248aff2277e5cdf1bc735b96f23ff6e9495
Yongbeom-Kim <dernbu@gmail.com>
2024-03-06 16:29:46 +0800

    feat(anki_hidden_cloze): Count cards rendered + automatically exit

--- automate_anki_hidden_cloze.sh
+++ automate_anki_hidden_cloze.sh
@@ -1,15 +1,30 @@
  
 #!/bin/bash
+# set -e
+
+n_notes=0
+
 while true
 do
-echo "loop"
-xdotool search --onlyvisible --classname "Anki" windowactivate --sync key --delay 20 e
-xdotool getactivewindow
-sleep 1
-xdotool getactivewindow
-xdotool key --delay 20 Escape
-sleep 1
-xdotool search --onlyvisible --classname "Anki" windowactivate --sync key --delay 20 equal
-xdotool getactivewindow
-sleep 4
+    xdotool search --onlyvisible --classname "Anki" windowactivate --sync key --delay 20 e
+    orig_window=$(xdotool getactivewindow)
+    # echo $orig_window
+
+    sleep 2
+
+    new_window=$(xdotool getactivewindow)
+    # echo $new_window
+
+    if [[ "$orig_window" == "$new_window" ]]; then
+        break
+    fi
+
+    xdotool key --delay 20 Escape
+    sleep 1
+    xdotool search --onlyvisible --classname "Anki" windowactivate --sync key --delay 20 equal
+    sleep 4
+
+    n_notes=$(($n_notes+1))
 done
+
+echo "$n_notes notes were rendered."
